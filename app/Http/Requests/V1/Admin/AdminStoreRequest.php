3816b6d381e53e163c49fde2205c892a90e8e373
JanayAlam <janayalam.work@gmail.com>
2023-09-08 01:46:45 +0600

    feat: create admin feature implemented with creating a middleware for checking if the authenticated user is an admin or not (recommit)

---
+++ app/Http/Requests/V1/Admin/AdminStoreRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\V1\Admin;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminStoreRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array {
+        return [
+            'user_id' => ['required']
+        ];
+    }
+}
---
+++ app/Http/Resources/V1/Admin/AdminResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\V1\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdminResource extends JsonResource {
+    /**
+     * Transform the resource into an array.
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array {
+        return [
+            'id' => $this->id,
+            'becameAdminAt' => $this->created_at->format('d-m-Y H:i:s'),
+        ];
+    }
+}
